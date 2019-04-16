ae8b8626c258c298e8759ab38119f7e3e531d446
robertnayael <nayael@wp.pl>
2019-04-16 22:14:18 +0200

    Add and style image list

---
+++ src/app/components/image-list/image-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styleUrls: [ './image-list.component.css' ]
+})
+export class ImageListComponent  {
+  @Input() images: string[];
+}
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,15 +1,18 @@
 import { ButtonComponent } from './button/button.component';
 import { ImageComponent } from './image/image.component';
+import { ImageListComponent } from './image-list/image-list.component';
 import { OverlayComponent } from './overlay/overlay.component';
 
 export {
   ButtonComponent,
   ImageComponent,
+  ImageListComponent,
   OverlayComponent
 };
 
 export default [
   ButtonComponent,
   ImageComponent,
+  ImageListComponent,
   OverlayComponent
 ];
